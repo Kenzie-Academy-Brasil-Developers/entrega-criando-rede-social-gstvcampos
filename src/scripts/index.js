dba71ca862436fbbfe30c7879d64b5e8ef9e4ed9
gstvcampos <gustavo.bmc76@gmail.com>
2023-04-05 12:17:46 -0300

    vascooo

--- src/scripts/index.js
+++ src/scripts/index.js
@@ -1,3 +1,8 @@
+import { users } from './database.js'
+import { posts } from './database.js'
+import { suggestUsers } from './database.js'
+
+
 function handleModal() {
     const buttons = document.querySelectorAll('.button__modal')
     const modal = document.querySelectorAll('.modal__container')
@@ -59,6 +64,51 @@ function like() {
         })
     }
 }
+
+function createSuggestUsers(array) {
+    const listUser = document.querySelector('.list__user')
+
+    listUser.innerHTML = ''
+
+    for (let i = 0; i < array.length; i++) {
+        const li = document.createElement('li')
+        const userItem = createUserItem(array[i])
+        const button = document.createElement('button')
+
+        button.innerText = 'Seguir'
+        button.classList.add('follow')
+        button.classList.add('follow-button')
+
+        li.append(userItem, button)
+        listUser.appendChild(li)
+    }
+}
+
+function createUserItem(user) {
+    const figure = document.createElement('figcaption')
+    const div1 = document.createElement('div')
+    const img = document.createElement('img')
+    const div2 = document.createElement('div')
+    const h2 = document.createElement('h2')
+    const p = document.createElement('p')
+
+    figure.classList.add('user__item')
+    div1.classList.add('user__img')
+    div2.classList.add('user__info')
+
+    img.src = user.img
+    img.alt = user.user
+    h2.innerText = user.user
+    p.innerText = user.stack
+
+    div1.appendChild(img)
+    div2.append(h2, p)
+    figure.append(div1, div2)
+
+    return figure
+}
+
+createSuggestUsers(suggestUsers)
 
 like()
 follow()
